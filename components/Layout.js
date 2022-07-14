a85bde4ace2fc76d21cbb7463c3d0e4910eb1834
Bishal Niroula <niroulabishal199@gmail.com>
2022-07-14 23:47:17 +0545

    new message

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,21 @@
 import Nav from "./Nav";
 import Footer from "./Footer";
+import Head from "next/head";
+import image from "../public/socialshareImage.png";
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <meta
+          property="og:title"
+          content="The Movies Library: Project on NextJS"
+        />
+        <meta
+          property="og:url"
+          content="https://movies-library-theta.vercel.app/"
+        />
+        <meta property="og:image" content={image} />
+      </Head>
       <Nav />
       {children}
       <Footer />
